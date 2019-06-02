2b3d4adac70257d6a0c48ca537af6c1d0e67b026
amalius-99 <amaliustapalov@gmail.com>
2019-06-02 19:05:41 +0200

    delete deprecated file

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import "./scss/index.scss";
+import "./js/coupon.js";
+import "./js/scripts.js";
+import "./js/terms.js";
+import "./js/terms_behaviors.js";
+import "./js/tweenmax.min.js";
+
+console.log("%c enjoy 😊!", "color: #ff0000");
