b1e04ddcf1282d908d436559cd1fe6d1eb1fd795
amalius-99 <amaliustapalov@gmail.com>
2019-06-02 19:07:04 +0200

    meh deploy

--- postcss.config.js
+++
@@ -1,7 +0,0 @@
-const autoprefixer = require("autoprefixer");
-const cssnano = require("cssnano");
-
-module.exports = {
-	sourceMap: true,
-	plugins: [autoprefixer({ grid: "autoplace" }), cssnano()]
-};
