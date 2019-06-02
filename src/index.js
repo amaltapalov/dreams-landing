d8be1e5c393d059607de15e70aa1c0ff86fbcadb
amalius-99 <amaliustapalov@gmail.com>
2019-06-02 20:34:47 +0200

    replace module Tweenmax to local script for GHP

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import "./js/coupon.js";
 import "./js/terms_behaviors.js";
 import "./js/terms.js";
 
-import "../node_modules/gsap/src/minified/TweenMax.min.js";
+// for GH pages
+import "./js/tweenmax.min.js";
+
+// for production
+// import "../node_modules/gsap/src/minified/TweenMax.min.js";
 
 console.log("%c U can find me on github @amaltapalov 😊!", "color: #6fd9c5");
