6af77b5663acb6753200bfb762c1e0d3b788313e
amalius-99 <amaliustapalov@gmail.com>
2019-06-02 20:00:54 +0200

    add TweenMax

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import "./scss/index.scss";
 import "./js/coupon.js";
 import "./js/terms_behaviors.js";
 import "./js/terms.js";
-console.log("%c enjoy 😊!", "color: #ff0000");
+import "../node_modules/gsap/src/minified/TweenMax.min.js";
+
+console.log("%c U can find me on github @amaltapalov 😊!", "color: #ff0000");
