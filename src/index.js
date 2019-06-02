47b590236c1b7a2860a7e67ef7582ef36def9c7e
amalius-99 <amaliustapalov@gmail.com>
2019-06-02 19:34:27 +0200

    delete duplicated script declaration

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import "./scss/index.scss";
 import "./js/coupon.js";
-import "./js/scripts.js";
-import "./js/terms.js";
 import "./js/terms_behaviors.js";
-import "./js/tweenmax.min.js";
-
+import "./js/terms.js";
 console.log("%c enjoy 😊!", "color: #ff0000");
